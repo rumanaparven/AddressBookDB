85a88e87f7249a840595b99676bd6a2873ebe6c4
Rumana <rumanaparven16@gmail.com>
2020-11-02 11:57:23 +0530

    first commit

--- AddressBookService.sql
+++ AddressBookService.sql
@@ -1,5 +1,5 @@
 --uc1 create database
-create database "AddressBookServiceDB"
+create database AddressBookServiceDB
 
 --uc2 create table
 CREATE table address_book(
@@ -52,3 +52,50 @@ select type,count(*) as Contacts from address_book group by type
 --uc11 add person to both friend and family
 insert into address_book values(
 'Neha','Gupta','abcdef','Kolkata','WB','234567','1234567890','nehagpt@gmail.com','neha','family')
+
+--uc12
+create table contact(
+ID int not null identity(1,1) primary key,
+firstname varchar(20) not null,
+lastname varchar(20) not null,
+phoneNumber varchar(20) not null,
+emailID varchar(30) not null
+)
+
+
+select * from contact
+insert into contact values (
+'Neha','Gupta','1234567890','nehagpt@gmail.com')
+insert into contact values (
+'Sunayana','Nag','9876543000','sunayana@gmail.com')
+insert into contact values (
+'Atul','Sharma','123456789','sharmaatul@gmail.com')
+insert into contact values (
+'Susmita','Sen','9330295337','susmita@gmail.com')
+
+
+create table address(
+ID int not null foreign key references contact(ID),
+name varchar(20) not null,
+type varchar(20) not null,
+address varchar(100),
+city varchar(100),
+state varchar(100),
+zip varchar(6)
+)
+select * from address
+
+insert into address values(
+1,'neha','friend','Newtown','Kolkata','WB','786598')
+insert into address values(
+2,'sunayana','office','Ramnagar','Agartala','Tripura','799260')
+insert into address values(
+3,'atul','friend',null,null,null,null)
+insert into address values(
+3,'susmita','family',null,null,null,null)
+
+update address set ID=4 where name='susmita'
+
+--uc13
+select c.firstname, c.lastname,c.phoneNumber,c.emailID,a.type,a.address,a.state from contact c
+join address a on c.id=a.id where type='friend'
